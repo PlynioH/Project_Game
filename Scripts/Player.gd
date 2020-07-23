148324a2d82510d4f027d78692853a40d9212bb9
Plynio Henrique Murat da Silva <66133284+PlynioH@users.noreply.github.com>
2020-07-23 02:18:32 -0300

    fix bugs
    fix infinity jump and others bugs
    
    create test area
    
    Co-Authored-By: Rafael Corrêa de Melo <36901020+lyem@users.noreply.github.com>

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -3,25 +3,37 @@ extends KinematicBody2D
 
 var speed = 200
 var run = 350
+var jump = 1000
+var jump_point = 1
+var released_jump = 1
 
 var velocity = Vector2()
 
 func get_input():
 	velocity.x = 0
 	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
+		velocity.x += speed
 	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
+		velocity.x -= speed
+	if Input.is_action_just_pressed("ui_up"):
+		if jump_point == 1:
+			velocity.y -= jump
+			jump_point = 0
+	if Input.is_action_just_released("ui_up"):
+		if released_jump == 1:
+			velocity.y = 30
+			released_jump = 0
 	if Input.is_action_pressed("ui_run"):
 		speed = run
 	if Input.is_action_just_released("ui_run"):
 		speed = 200
-		
+	if Input.is_action_just_pressed("reload"):
+		get_tree().reload_current_scene()
+
 func gravity():
 	if is_on_floor():
-		pass
+		jump_point = 1
+		released_jump = 1
 	else:
 		velocity.y += 30
 
