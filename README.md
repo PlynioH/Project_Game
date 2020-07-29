7b4796783a03ad1c0f7070dc200080f2e306af95
rafel corrêa de melo <rfael403@gmail.com>
2020-07-29 01:46:41 -0300

    reiniciando

--- README.md
+++
@@ -1,2 +0,0 @@
-# Project_Game
-This is my first game in 2D plataform, I and my friend will start today 22/07/2020.
--- Scripts/main.gd
+++
@@ -1,7 +0,0 @@
-extends KinematicBody2D
-
-
-func _physics_process(_delta):
-	if Input.is_action_just_pressed("reload"):
-# warning-ignore:return_value_discarded
-		get_tree().reload_current_scene()
